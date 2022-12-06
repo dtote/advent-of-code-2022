b1450d99bf59090d833d48b57fb9e1dba76dc5a1
Nestor Torres <alu0101021768@ull.edu.es>
2022-12-06 23:24:37 +0000

    feat(day-5): added day 5 solutions

---
+++ day-05/part-2.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import { Reader } from "../shared/reader";
+
+const FILE_PATH = path.resolve(__dirname, "..", "..", "day-05");
+const FILE_NAME = "input.txt";
+
+const reader = Reader.create(FILE_PATH, FILE_NAME);
+const input: string = reader.run();
+
+const [rawElements, rawInstructions] = input.split("\n\n")
+const instructions = rawInstructions.split("\n").map((instruction) => instruction.replace(/[^0-9]/g, " ").replace(/ +/g, " ").split(" ").filter(Boolean).map(Number))
+const elementsAndColumns = rawElements.split("\n")
+const columnsIndex = elementsAndColumns.length - 1;
+const elementsRows = elementsAndColumns.slice(0, columnsIndex)
+const columns = elementsAndColumns[columnsIndex]
+const columnsRowCharacters = columns.split("");
+
+const eachRow = columns.replace(/ +/g, "").split("")
+const columnItems: Record<string, string> = Object.fromEntries(eachRow.map((element) => ([[element], ""])))
+
+// Using an object as a stack for the elements on each row
+columnsRowCharacters.forEach((character, columnIndex) => {
+	const isColumn = character.match(/[0-9]/)
+	if (!isColumn) return
+
+	elementsRows.forEach((row) => {
+		const elementAtColumnIndex = row.at(columnIndex)
+		if (!elementAtColumnIndex || !elementAtColumnIndex.match(/[A-Z]/)) return
+
+		columnItems[character] += elementAtColumnIndex
+	})
+})
+
+instructions.forEach(([elementsToMove, from, to]) => {
+	columnItems[to] = columnItems[from].slice(0, elementsToMove) + columnItems[to]
+	columnItems[from] = columnItems[from].slice(elementsToMove)
+})
+
+const finalMessage = Object.values(columnItems).map(([firstCharacter]) => firstCharacter).join("")
+console.log({  finalMessage } )
